6c9ea409181883a139f00c9a1c80d54c4d9de654
schneppd <david.schnepp@schneppd.com>
2016-09-15 19:39:28 +0200

    Refactor modulecontroller, simplify logic for module cache creation.

--- src/framework/modulecontroller.php
+++ src/framework/modulecontroller.php
@@ -12,7 +12,6 @@ abstract class ModuleController {
 	protected static $model = null;
 	protected static $isInitialized = false;
 	protected static $moduleInformations = null;
-	protected static $isCacheCreated = false;
 
 	public function __construct($infos) {
 		if(!static::$isInitialized){
@@ -68,35 +67,24 @@ abstract class ModuleController {
 	}
 	
 	public static function installModuleCache() {
-		$dir = static::$moduleInformations->getModuleCachePath();
-		if(ModuleIO::existDirectory($dir))
+		if(static::isModuleCacheCreated())
 			return true;
+		$dir = static::$moduleInformations->getModuleCachePath();
 		static::$isCacheCreated = ModuleIO::createDirectory($dir);
-		return static::$isCacheCreated;
 	}
 	
 	public static function uninstallModuleCache() {
-		$dir = static::$moduleInformations->getModuleCachePath();
-		if(ModuleIO::existDirectory($dir))
+		if(static::isModuleCacheCreated()) {
+			$dir = static::$moduleInformations->getModuleCachePath();
 			ModuleIO::emptyAndDeleteDirectory($dir);
-		static::$isCacheCreated = false;
+		}
 		return true;
 	}
 	
 	public static function isModuleCacheCreated() {
-		return static::$isCacheCreated;
+		$dir = static::$moduleInformations->getModuleCachePath();
+		return ModuleIO::existDirectory($dir);
 	}
 	
-	/*
-	 * write a string to a file in module's cache
-	 * return true if no error, or else if something went wrong
-	 */
-	public static function writeStringToModuleCache($moduleName, $file, $str) {
-		$cacheFilePath = self::getModuleCachePath($moduleName) . '/' . $file;
-		$wCacheFile = fopen($cacheFilePath, "w") or die("Unable to open cache!");
-		fwrite($wCacheFile, $str);
-		fclose($wCacheFile);
-		return true; //success
-	}
 
 }
