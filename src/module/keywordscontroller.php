34cdd11ffdea16951f7f98cdc5f6d3551b1a9546
schneppd <david.schnepp@schneppd.com>
2016-09-15 22:09:30 +0200

    Debug canDisplayTagList.

--- src/module/keywordscontroller.php
+++ src/module/keywordscontroller.php
@@ -47,7 +47,7 @@ class KeywordsController extends \NsC3Framework\ModuleController {
 	public function canDisplayTagList(&$id_category) {
 		$file = 'c3keywords_' . $id_category. '.cache';
 		$filePath = static::$moduleInformations->getModuleCacheFilePath($file);
-		return ModuleIO::existFile($filePath);
+		return \NsC3Framework\ModuleIO::existFile($filePath);
 	}
 
 }
